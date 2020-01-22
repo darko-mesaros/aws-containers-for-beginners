92a74b557a82029013a1cb404eb3f9fbbf88c347
darko-mesaros <darko.subotica@gmail.com>
2020-01-22 17:27:49 +0200

    Hello

--- infra/terraform-state.tf
+++ infra/terraform-state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "webinar-terraform-sample"
+    bucket  = "dmeszaro-tf-statebucket01"
     key     = "statefiles"
     region  = "eu-west-1"
     profile = "aws-webinar"
